ffaf6d39457d4d4b75c8461e6191f1b9318ae86d
陈明磊 <chenml0906@126.com>
2019-06-27 21:17:42 +0800

    Update __main__.py
    add some paramters ,for 
    -G (json: one table as json array or line : row of table as json object )
    -b batchsize performance paramter, using fetchmany

--- mysqljson/__main__.py
+++ mysqljson/__main__.py
@@ -10,7 +10,7 @@ import argparse
 import getpass
 import json
 import MySQLdb
-import sys
+import sys,os
 
 def cursor_to_dict(cursor):
     data = cursor.fetchone()
@@ -27,6 +27,22 @@ def cursor_to_dict(cursor):
 
     return result
 
+def cursor_to_dicts(cursor,batch_size=1000):
+    datas = cursor.fetchmany(batch_size)
+
+    if data is None:
+        return None
+
+    desc = cursor.description
+    results = []
+    result = {}
+    for data in datas:
+        for (name, value) in zip(desc, data):
+            result[name[0]] = value
+        results.append(result)
+
+    return results
+
 def main():
     arg_parser = argparse.ArgumentParser()
 
@@ -35,6 +51,8 @@ def main():
     arg_parser.add_argument('-P', '--port', help='MySQL port number.', default=3306, type=int)
     arg_parser.add_argument('-u', '--user', help='MySQL username.', default='root')
     arg_parser.add_argument('-p', '--password', help='Shh! It\'s a secret.', action='store_true')
+    arg_parser.add_argument('-b', '--batchsize', help='using fetchmany to get data',  default= 1 , type=int)
+    arg_parser.add_argument('-G', '--jsonarray', help='json(one table as json array) or line(row of table as json object write to text line)',  default='json')
     arg_parser.add_argument('-e', '--query', help='Query to run.', required=True)
 
     args = arg_parser.parse_args()
@@ -48,31 +66,58 @@ def main():
 
     try:
         cursor = conn.cursor()
-
+        # SET query encode
+        cursor.execute("SET NAMES utf8")
+        conn.commit();
+        
         cursor.execute(args.query)
     except MySQLdb.Error as e:
         sys.stderr.write('MySQL Error [{}]: {}\n'.format((e.args[0], e.args[1])))
         sys.exit()
-
-    sys.stdout.write('[')
-
-    row = cursor_to_dict(cursor)
-
-    first_line = True
-
-    while row is not None:
-        if first_line == True:
-            first_line = False
-        else:
-            sys.stdout.write(',')
-
-        json_str = json.dumps(row, default=str)
-
-        sys.stdout.write(json_str)
-
+    if args.jsonarray == 'json':
+        sys.stdout.write('[')
+    if args.batchsize == 1:
         row = cursor_to_dict(cursor)
+        first_line = True
+        while row is not None:
+            if first_line == True:
+                first_line = False
+            else:
+                if args.jsonarray == 'json':
+                    sys.stdout.write(',')
+                    sys.stdout.write(os.linesep)
+                else:
+                    sys.stdout.write(os.linesep)
 
-    sys.stdout.write(']')
+            json_str = json.dumps(row, default=str, ensure_ascii=False)
+
+            sys.stdout.write(json_str)
+
+            row = cursor_to_dict(cursor)
+        if args.jsonarray == 'json':
+            sys.stdout.write(']')
+     else:
+        rows = cursor_to_dicts(cursor,args.batchsize)
+        first_line = True
+        while rows is not None:
+            for row in rows:
+                if first_line == True:
+                    first_line = False
+                else:
+                    if args.jsonarray == 'json':
+                        sys.stdout.write(',')
+                        sys.stdout.write(os.linesep)
+                    else:
+                        sys.stdout.write(os.linesep)
+
+                json_str = json.dumps(row, default=str, ensure_ascii=False)
+
+                sys.stdout.write(json_str)
+
+            rows = cursor_to_dicts(cursor,args.batchsize)
+        if args.jsonarray == 'json':
+            sys.stdout.write(']')
+        
 
 if __name__ == "__main__":
     main()
